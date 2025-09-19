7c6f6021a0fd9764bd2d032b52db307d021d3842
yanhaoxiang565 <ljm060412@qq.com>
2025-09-19 11:03:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-input_str = input().strip()
-if len(input_str) != 5 or not input_str.isdigit():
-    print("错误")
+n = input('请输入一个5位数字: ')
+if len(n) != 5 or not n.isdigit():
+    print('输入错误: 请输入5位数字')
 else:
-    if input_str == input_str[::-1]:
-        print("是回文数")
+    if n == n[::-1]:
+        print('是回文数')
     else:
-        print("不是回文数")
+        print('不是回文数')
