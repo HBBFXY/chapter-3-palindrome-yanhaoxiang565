4f6ca0c75eac3e27e26d4439d9eeed706c8e0601
yanhaoxiang565 <ljm060412@qq.com>
2025-09-19 11:00:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-# 获取用户输入并去除前后空格
 input_str = input().strip()
-
-# 检查输入是否为5位且全为数字
 if len(input_str) != 5 or not input_str.isdigit():
     print("错误")
 else:
-    # 反转字符串并与原字符串比较
     if input_str == input_str[::-1]:
         print("是回文数")
     else:
