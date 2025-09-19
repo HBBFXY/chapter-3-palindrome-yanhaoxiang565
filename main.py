c04d58e666ce9f2793ffcc5ede7907ad162b0f45
yanhaoxiang565 <ljm060412@qq.com>
2025-09-19 10:37:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-# 从键盘获取输入
-num = input("请输入一个5位数字：")
-
-# 检查是否为5位且全为数字
+num = input()
 if len(num) != 5 or not num.isdigit():
     print("错误提示：请输入一个5位数字")
 else:
-    # 判断是否为回文数
     if num == num[::-1]:
         print("是回文数")
     else:
